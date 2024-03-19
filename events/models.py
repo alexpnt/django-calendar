a56bcfd59d22ba337324c62b7cff21ed965233bc
Muthu Annamalai <photonique@hey.com>
2024-03-19 11:15:54 -0700

    - update the codebase to Django 4.2 - update to absolute package imports for Python3.x standards

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.core.urlresolvers import reverse
+from django.shortcuts import reverse
 
 # Create your models here.
 
